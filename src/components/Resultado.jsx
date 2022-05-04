50d6575a2c3cdc8b63ee7ca51c1cd8e80f95f60d
Jesús Guerrero <jesus.guerrero@talan.com>
2022-05-04 17:41:59 +0200

    Finalizando el proyecto y añadiendo hooks

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,14 +1,57 @@
+import { useMemo, useRef } from 'react'
 import useCotizador from "../hooks/useCotizador";
+import { MARCAS, PLANES } from '../constants';
 
 const Resultado = () => {
 
-    const { resultado } = useCotizador();
+    const { resultado, datos } = useCotizador();
+    const { marca, plan, year } = datos
+    const yearRef = useRef(year)
+
+    // Usando useCallback
+    // const [nombreMarca] = useCallback(
+    //     MARCAS.filter(m => m.id === Number(marca)), 
+    // [resultado])
+
+    // const [nombrePLan] = useCallback(
+    //     PLANES.filter(p=> p.id === Number(plan)), 
+    // [resultado])
+
+    const [nombreMarca] = useMemo(() =>
+        MARCAS.filter(m => m.id === Number(marca)), 
+    [resultado])
+
+    const [nombrePLan] = useMemo( () =>
+        PLANES.filter(p=> p.id === Number(plan)), 
+    [resultado])
 
     if (resultado === 0) return null;
 
     return (
-        <div>
-            {resultado}
+        <div className="bg-gray-100 text-center mt-5 p-5 shadow">
+            <h2 className="text-gray-600 font-black text-3xl">
+                Resumen
+            </h2>
+
+            <p className="my-2">
+                <span className="font-bold">Marca: </span>
+                {nombreMarca.nombre}
+            </p>
+
+            <p className="my-2">
+                <span className="font-bold">Plan: </span>
+                {nombrePLan.nombre}
+            </p>
+
+            <p className="my-2">
+                <span className="font-bold">Año del Coche: </span>
+                {yearRef.current}
+            </p>
+
+            <p className="my-2 text-2xl">
+                <span className="font-bold">Total Cotización: </span>
+                {resultado}
+            </p>
         </div>
     )
 }
