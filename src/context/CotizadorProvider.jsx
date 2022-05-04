7da3a440b388b24f740959e9632010dcee273df1
Jesús Guerrero <jesus.guerrero@talan.com>
2022-05-04 13:44:31 +0200

    Comenzando el proyecto

---
+++ src/context/CotizadorProvider.jsx
@@ -0,0 +1,42 @@
+import { useState, createContext } from 'react';
+
+const CotizadorContext = createContext();
+
+const CotizadorProvider = ({children}) => {
+
+    const [datos, setDatos] = useState({
+        marca: '',
+        year: '',
+        plan: ''
+    })
+
+    const [error, setError] = useState('');
+
+    const handleChangeDatos = e => {
+        //Copia del state y reescribe para no perder datos
+        setDatos({
+            ...datos,
+            [e.target.name]: e.target.value
+        })
+    }
+
+
+    return (
+        <CotizadorContext.Provider
+            value={{
+                datos,
+                handleChangeDatos,
+                error,
+                setError
+            }}
+        >
+            {children}
+        </CotizadorContext.Provider>
+    )
+}
+
+export {
+    CotizadorProvider
+}
+
+export default CotizadorContext
